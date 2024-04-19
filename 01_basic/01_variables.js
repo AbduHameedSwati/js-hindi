71749067dcf14a505af2c8f294c9edad633506e4
AbduHameedSwati <167334128+AbduHameedSwati@users.noreply.github.com>
2024-04-19 11:05:53 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 13444
+let accountEmail = 'hameed22@gmail.com'
+let accountPassword = '12345'
+accountCity = 'karachi'
+// account = 2
+
+
+accountEmail = 'A.H.Swati143@gmail.com'
+accountPassword = '143786'
+accountCity = 'mahansera'
+
+
+console.table([accountId, accountCity, accountEmail, accountPassword])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all jsb codse as newer version
+
+let name = 'hitesh'
+let age = 15
+let loggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string+> ""
+//boolean => true/False
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+// console.log(typeof "abdulhameed");
+// console.log(typeof undefined) //undefined console show
+// console.log(typeof null) // show console object
