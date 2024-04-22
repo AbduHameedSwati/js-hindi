9ad28a1fe32a4e43f1f8d92b8bc9862264cdae3c
AbduHameedSwati <167334128+AbduHameedSwati@users.noreply.github.com>
2024-04-22 08:54:07 +0000

    conversion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = null
+
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// true=> 1; false =>0
+
+let isLoggedIn = "1"
+
+let booleanIsLoggendIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggendIn)
+
+
+let someNumber = 33
+
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum);
