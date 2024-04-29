3f77dd543774a830ca0c15e77196875069a29dbd
AbduHameedSwati <167334128+AbduHameedSwati@users.noreply.github.com>
2024-04-29 13:54:12 +0000

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -22,3 +22,35 @@ let someNumber = 33
 let stringNum = String(someNumber)
 console.log(stringNum);
 console.log(typeof stringNum);
+
+// ........ Operation...............
+
+let value = 3 
+let negValue = +value
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2**3);
+// console.log(2*3);
+// console.log(2/3);
+
+let str1 = "hello"
+let str2 = "A.Hameed"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 1);
+console.log(1+4+"3");
+
+console.log((3+4)*5%3);
+
+console.log(+"");
+
+let gameCounter = 101
+gameCounter++
+console.log(gameCounter);
